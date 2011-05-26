6ba866f8343a85bf6a4d997fc2585cd1478e6e39
Toni Grigoriu <toni@tonigrigoriu.com>
2011-05-26 13:42:32 +0200

    move send header logic to observer

---
+++ code/Helper/Data.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Data helper
+ *
+ * @category    Aoe
+ * @package     Aoe_Static
+ * @author      Toni Grigoriu <toni@tonigrigoriu.com>
+ */
+class Aoe_Static_Helper_Data extends Mage_Core_Helper_Abstract
+{
+    /**
+	 * Check if a fullActionName is configured as cacheable
+	 *
+	 * @param string $fullActionName
+	 * @return false|int false if not cacheable, otherwise lifetime in seconds
+	 */
+	public function isCacheableAction($fullActionName)
+    {
+		$cacheActionsString = Mage::getStoreConfig('system/aoe_static/cache_actions');
+		foreach (explode(',', $cacheActionsString) as $singleActionConfiguration) {
+			list($actionName, $lifeTime) = explode(';', $singleActionConfiguration);
+			if (trim($actionName) == $fullActionName) {
+				return intval(trim($lifeTime));
+			}
+		}
+		return false;
+	}
+}
---
+++ code/Model/Observer.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Observer model
+ *
+ * @category    Aoe
+ * @package     Aoe_Static
+ * @author      Toni Grigoriu <toni@tonigrigoriu.com>
+ */
+class Aoe_Static_Model_Observer
+{
+    /**
+     * Check when varnish caching should be enabled.
+     *
+     * @param Varien_Event_Observer $observer
+     * @return Aoe_Static_Model_Observer
+     */
+    public function processPreDispatch(Varien_Event_Observer $observer)
+    {
+        /* @var $helper Aoe_Static_Helper_Data */
+        $helper = Mage::helper('aoestatic');
+        
+        $controllerAction = $observer->getEvent()->getControllerAction();
+        
+        /* @var $request Mage_Core_Controller_Request_Http */
+        $request = $controllerAction->getRequest();
+        /* @var $response Mage_Core_Controller_Response_Http */
+        $response = $controllerAction->getResponse();
+        
+        $fullActionName = $controllerAction->getFullActionName();
+        
+        $lifetime = $helper->isCacheableAction($fullActionName);
+        
+        if (!$lifetime) {
+            return $this;
+        }
+        
+        $response->setHeader('X-Magento-Lifetime', $lifetime, true); // Only for debugging and information
+		$response->setHeader('X-Magento-Action', $fullActionName, true); // Only for debugging and information
+        $response->setHeader('Cache-Control', 'max-age='. $lifetime, true);
+    	$response->setHeader('aoestatic', 'cache', true);
+		
+        return $this;
+    }
+}
