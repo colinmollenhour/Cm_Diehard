26fc9f72667ad6e56b7571dc33e97daf45b0b111
Fabrizio Branca <github@fabrizio-branca.de>
2011-06-20 21:40:31 +0200

    Removed check for XmlHttpRequest

--- code/controllers/CallController.php
+++ code/controllers/CallController.php
@@ -18,7 +18,7 @@ class Aoe_Static_CallController extends Mage_Core_Controller_Front_Action {
 	 */
 	public function indexAction() {
 
-		if (!$this->getRequest()->isXmlHttpRequest()) { Mage::throwException('This is not an XmlHttpRequest'); }
+		// if (!$this->getRequest()->isXmlHttpRequest()) { Mage::throwException('This is not an XmlHttpRequest'); }
 
 		$response = array();
 		$response['sid'] = Mage::getModel('core/session')->getEncryptedSessionId();
