773c4937277dbbf6d2dba14df023fef892d064a0
Vinai Kopp <vinai@netzarbeiter.com>
2014-06-03 16:50:07 +0200

    Properly handle the return values for the ignored blocks

--- code/Model/Observer.php
+++ code/Model/Observer.php
@@ -59,7 +59,7 @@ class Cm_Diehard_Model_Observer
             ));
 
             // Update ignored blocks cookie
-            $ignored = (array) $this->helper()->getIgnoredBlocks();
+            $ignored = $this->helper()->getIgnoredBlocks();
             if ($ignored === NULL) {
                 $ignored = $this->helper()->getDefaultIgnoredBlocks();
             }
